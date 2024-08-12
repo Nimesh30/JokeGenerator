509bcbe4cfa48365cf336d702595fad64c9ac5a3
Nimesh30 <nimprajapati2004@gmail.com>
2024-08-12 18:59:03 +0530

    Build responsive joke generator

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+document.getElementById('jokeBtn').addEventListener('click', async () => {
+    const jokeDisplay = document.getElementById('jokeDisplay');
+    jokeDisplay.innerHTML = "Loading...";
+
+    try {
+        const response = await fetch('https://v2.jokeapi.dev/joke/Any');
+        const data = await response.json();
+
+        if (data.type === 'single') {
+            jokeDisplay.innerHTML = `<p>${data.joke}</p>`;
+        } else {
+            jokeDisplay.innerHTML = `<p>${data.setup}</p><p>${data.delivery}</p>`;
+        }
+    } catch (error) {
+        jokeDisplay.innerHTML = `<p>Sorry, something went wrong. Please try again later.</p>`;
+    }
+});
