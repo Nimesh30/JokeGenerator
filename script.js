4c4d35393ae410002bddbb98191ac34374e0468f
Nimesh30 <nimprajapati2004@gmail.com>
2024-08-18 22:19:21 +0530

    Add speech joke button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // script.js
 const jokeBtn = document.getElementById("jokeBtn");
 const copyBtn = document.getElementById("copyBtn");
+const speakbtn =document.getElementById("speak");
  
 function theme() {
   document.body.classList.toggle('dark');
@@ -21,6 +22,8 @@ jokeBtn.addEventListener("click", async () => {
       jokeDisplay.innerHTML = `<p>${data.setup}</p><p>${data.delivery}</p>`;
     }
     copyBtn.removeAttribute("hidden");
+    speakbtn.removeAttribute("hidden");
+    
   } catch (error) {
     jokeDisplay.innerHTML = `<p>Sorry, something went wrong... Please try again later.</p>`;
   } finally {
@@ -32,3 +35,10 @@ copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(jokeDisplay.innerText);
   alert("Joke copied to clipboard.");
 });
+
+speakbtn.addEventListener('click',()=>{
+  const speakjoke = jokeDisplay.innerText;
+  const speech = new SpeechSynthesisUtterance(speakjoke);
+  window.speechSynthesis.speak(speech);
+});
+
